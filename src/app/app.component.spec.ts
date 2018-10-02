b8348935840521de378b6e00fd2b2a6069ecbddd
ginosian <marta.ginosian@gmail.com>
2018-10-02 16:50:30 +0400

    Initial commit.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'at-front'`, () => {
+  it(`should have as title 'at-front-test'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('at-front');
+    expect(app.title).toEqual('at-front-test');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to at-front!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to at-front-test!');
   });
 });
